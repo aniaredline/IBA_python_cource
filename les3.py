44a7bb14e0773cd4fb853c12d67ad7954b248eb3
aniaredline <valendaana@gmail.com>
2021-11-01 20:35:52 +0300

    les3 partly

--- les3.py
+++ les3.py
@@ -1,4 +1,28 @@
-def spa(name, age):
-    print('my name is',  name, ',',  'my age is', age)
+import random
 
-spa('Ana', '28')
+row, col = 5, 5
+matrix_ = [[random.randint(-1000, 1000) for _ in range(col)] for _ in range(row)]
+matrix = [
+    [1, 2, 3, 4, 5],
+    [2, 1, 6, 7, 8],
+    [3, 6, 1, 9, 10],
+    [4, 7, 9, 1, 11],
+    [5, 8, 10, 11, 1]
+]
+
+
+def symmetric(matrix):
+    sym = True
+    for j in range(row):
+        for i in range(col):
+            if matrix[j][i] != matrix[i][j]:
+                sym = False
+                break
+        if not sym:
+            break
+
+
+print(sym)
+
+if __name__ == "__name__":
+    pass
