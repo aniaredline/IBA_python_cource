138b62de1d7cefd774947f52d295fc25f5299cb4
aniaredline <valendaana@gmail.com>
2021-11-01 23:28:02 +0300

    finally

--- les3.py
+++ les3.py
@@ -1,28 +1,37 @@
-import random
+def symmetric(lst):
+    row = len(lst)
 
-row, col = 5, 5
-matrix_ = [[random.randint(-1000, 1000) for _ in range(col)] for _ in range(row)]
-matrix = [
-    [1, 2, 3, 4, 5],
-    [2, 1, 6, 7, 8],
-    [3, 6, 1, 9, 10],
-    [4, 7, 9, 1, 11],
-    [5, 8, 10, 11, 1]
-]
+    if row == 0:
+        return False
 
+    col = len(lst[0])
 
-def symmetric(matrix):
-    sym = True
+    if row != col:
+        return False
+
     for j in range(row):
         for i in range(col):
-            if matrix[j][i] != matrix[i][j]:
-                sym = False
-                break
-        if not sym:
-            break
+            if lst[j][i] != lst[i][j]:
+                return False
+    return True
 
 
-print(sym)
+if __name__ == "__main__":
+    matrix_symm = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 6, 7, 8],
+        [3, 6, 1, 9, 10],
+        [4, 7, 9, 1, 11],
+        [5, 8, 10, 11, 1]
+    ]
 
-if __name__ == "__name__":
-    pass
+    matrix_unsymm = [
+        [1, 2, 3, 4, 5],
+        [2, 1, 6, 7, 8],
+        [3, 6, 1, 9, 10],
+        [4, 7, 9, 1, 11],
+        [5, 8, 11, 11, 1]
+    ]
+
+    print(f'Is matrix symmetric? {symmetric(matrix_symm)}')
+    print(f'Is matrix symmetric? {symmetric(matrix_unsymm)}')
